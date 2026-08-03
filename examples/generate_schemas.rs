a3ce1c664d7b44f6c19bd180a499a930969c066d
Alexander Mills <alex@oresoftware.com>
2026-08-03 00:21:01 -0500

    feat: generate OCI adapter contract schema

--- examples/generate_schemas.rs
+++ examples/generate_schemas.rs
@@ -24,6 +24,7 @@ fn main() {
     write::<zed_interfaces::Lockfile>(dir, "lockfile");
     write::<zed_interfaces::NixExportSection>(dir, "nix-export-section");
     write::<zed_interfaces::NixAdapterRecord>(dir, "nix-adapter-record");
+    write::<zed_interfaces::OciAdapterRecord>(dir, "oci-adapter-record");
     write::<zed_interfaces::registry::PackageMetadata>(dir, "package-metadata");
     write::<zed_interfaces::registry::VersionMetadata>(dir, "version-metadata");
     write::<zed_interfaces::registry::PublishMeta>(dir, "publish-meta");
